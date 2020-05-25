1e68abbaf7e5c4fca01b12a518f4e239f1d209b9
Rudá Torquato <rudatorquato@gmail.com>
2020-05-25 12:06:39 -0300

    Pasta appshow com tentativa da primeira Activity e o Controller ja ok

---
+++ app/src/com/example/appshow/ControllerActions.java
@@ -0,0 +1,8 @@
+package com.example.appshow;
+
+public abstract class ControllerActions {
+
+    public void onRightSwiped(int position) {}
+    public void onLeftSwiped(int position) {}
+    public void onHourSet(int position, String data) {}
+}
