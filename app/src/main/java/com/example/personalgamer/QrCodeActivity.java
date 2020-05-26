2113b9d131bccf465959b78a83d992fed9c80d69
Leon <leandrobruto42@gmail.com>
2020-05-26 01:07:00 -0300

    qrcode activity

--- app/src/main/java/com/example/personalgamer/QrCodeActivity.java
+++ app/src/main/java/com/example/personalgamer/QrCodeActivity.java
@@ -1,6 +1,7 @@
 package com.example.personalgamer;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class QrCodeActivity extends AppCompatActivity {
@@ -8,5 +9,6 @@ public class QrCodeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
     }
 }
