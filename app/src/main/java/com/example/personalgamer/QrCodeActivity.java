893cfbe242301e17c1571dacbf9407cfd07b0c52
Rudá Torquato <rudatorquato@gmail.com>
2020-05-26 16:49:12 -0300

    Leitura do qrcode

--- app/src/main/java/com/example/personalgamer/QrCodeActivity.java
+++ app/src/main/java/com/example/personalgamer/QrCodeActivity.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class QrCodeActivity extends AppCompatActivity {
+import com.google.zxing.Result;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+//Responsavel pelo scan
+public class QrCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+
+    ZXingScannerView ScannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qr_code);
+        ScannerView = new ZXingScannerView(this);
+        //setContentView(R.layout.activity_qr_code);
+        setContentView(ScannerView);
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        TreinandoActivity.resultado.setText(result.getText());
+        onBackPressed();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        ScannerView.stopCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ScannerView.setResultHandler(this);
+        ScannerView.startCamera();
     }
 }
---
+++ app/src/main/java/com/example/personalgamer/TreinandoActivity.java
@@ -0,0 +1,34 @@
+package com.example.personalgamer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TreinandoActivity extends AppCompatActivity {
+
+   public static TextView resultado;
+    Button scan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
+
+        resultado = (TextView)findViewById(R.id.resultado);
+        scan = (Button) findViewById(R.id.scan);
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(), QrCodeActivity.class ));
+
+            }
+        });
+    }
+}
+
