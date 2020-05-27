febedb346bedba2e01ed44dea4c56793f72992c7
Leon <leandrobruto42@gmail.com>
2020-05-27 03:55:08 -0300

    on item clicked e cadastro de exercicios

--- app/src/main/java/adapter/ExerciciosAdapter.java
+++ app/src/main/java/adapter/ExerciciosAdapter.java
@@ -6,16 +6,19 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.personalgamer.R;
 
 import java.util.List;
 
+import interfaces.OnItemClicked;
 import modelo.Training;
 
 public class ExerciciosAdapter extends RecyclerView.Adapter<ExerciciosAdapter.ExerciciosViewHolder> {
     private List<Training> trainings;
+    private OnItemClicked onClick;
 
     public ExerciciosAdapter(List<Training> trainings) {
         this.trainings = trainings;
@@ -23,6 +26,7 @@ public class ExerciciosAdapter extends RecyclerView.Adapter<ExerciciosAdapter.Ex
 
     static class ExerciciosViewHolder extends RecyclerView.ViewHolder {
         private TextView tv_nome;
+        private CardView card_user;
 
         ExerciciosViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -45,10 +49,15 @@ public class ExerciciosAdapter extends RecyclerView.Adapter<ExerciciosAdapter.Ex
         Training training = trainings.get(position);
 
         holder.tv_nome.setText(training.getTraining());
+        holder.tv_nome.setOnClickListener(v -> onClick.onItemClick(v, position));
     }
 
     @Override
     public int getItemCount() {
         return trainings.size();
+    }
+
+    public void setOnClick(OnItemClicked onClick) {
+        this.onClick = onClick;
     }
 }
--- app/src/main/java/com/example/personalgamer/ExerciciosActivity.java
+++ app/src/main/java/com/example/personalgamer/ExerciciosActivity.java
@@ -5,18 +5,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TableRow;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import adapter.ExerciciosAdapter;
+import interfaces.OnItemClicked;
 import modelo.Training;
 
-public class ExerciciosActivity extends AppCompatActivity implements View.OnClickListener {
+public class ExerciciosActivity extends AppCompatActivity implements View.OnClickListener, OnItemClicked {
     private Context context = this;
+    private Button btn_cadastrar;
     private ExerciciosAdapter exerciciosAdapter;
 
     @Override
@@ -24,11 +28,18 @@ public class ExerciciosActivity extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercicios);
 
+        loadViews();
+
         ArrayList<Training> trainings = new ArrayList<>();
         trainings.add(new Training("dasd", "dasda","dasdas","dasda",0303, 9494,49949,4004));
         trainings.add(new Training("dasd", "dasda","dasdas","dasda",0303, 9494,49949,4004));
         trainings.add(new Training("dasd", "dasda","dasdas","dasda",0303, 9494,49949,4004));
         updateRecycleView(trainings);
+    }
+
+    private void loadViews() {
+        btn_cadastrar = findViewById(R.id.btn_cadastrar);
+        btn_cadastrar.setOnClickListener(this);
     }
 
     private void updateRecycleView(List<Training> trainings) {
@@ -39,11 +50,20 @@ public class ExerciciosActivity extends AppCompatActivity implements View.OnClic
         recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(exerciciosAdapter);
         registerForContextMenu(recyclerView);
-//        exerciciosAdapter.setOnClick(ExerciciosActivity.this);
+        exerciciosAdapter.setOnClick(ExerciciosActivity.this);
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.btn_cadastrar) {
+            startActivity(new Intent(context, CadastraExercicioActivity.class)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+        }
+    }
 
+    @Override
+    public void onItemClick(View view, int position) {
+        startActivity(new Intent(context, InfoTreinoActivity.class)
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
     }
 }
