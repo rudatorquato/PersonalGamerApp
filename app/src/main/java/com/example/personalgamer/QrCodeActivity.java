3ae724824c24ae73a274282920dc8fe923821290
Leon <leandrobruto42@gmail.com>
2020-05-28 11:18:03 -0300

    camera permission request

--- app/src/main/java/com/example/personalgamer/QrCodeActivity.java
+++ app/src/main/java/com/example/personalgamer/QrCodeActivity.java
@@ -1,18 +1,25 @@
 package com.example.personalgamer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.zxing.Result;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class QrCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler, View.OnClickListener {
     private Context context = this;
+    private static final int CAMERA_REQUEST_CODE = 100;
     private Button btn_comecar;
 
     ZXingScannerView ScannerView;
@@ -57,9 +64,30 @@ public class QrCodeActivity extends AppCompatActivity implements ZXingScannerVie
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btn_comecar) {
-            setContentView(ScannerView);
-            ScannerView.setResultHandler(this);
-            ScannerView.startCamera();
+            checkCameraPermission();
+        }
+    }
+
+    private void checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED);
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+
+                setContentView(ScannerView);
+                ScannerView.setResultHandler(this);
+                ScannerView.startCamera();
+
+            } else {
+//                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
