ecd8bbffba249717e357ebed1d42d5bbf7d1676f
Leon <leandrobruto42@gmail.com>
2020-05-28 12:46:57 -0300

    login e cadastro de user

--- app/src/main/java/com/example/personalgamer/CadastroActivity.java
+++ app/src/main/java/com/example/personalgamer/CadastroActivity.java
@@ -1,13 +1,22 @@
 package com.example.personalgamer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Map;
 
 import interfaces.NetworkObserver;
 import network.NetworkManager;
@@ -15,10 +24,14 @@ import util.Path;
 
 public class CadastroActivity extends AppCompatActivity implements View.OnClickListener {
     private Context context = this;
+    private TextInputEditText edt_user, edt_email, edt_user_type;
     private Button sign_in_button, sign_up_button;
+    private CoordinatorLayout coordinatorLayout;
 
     private NetworkManager manager;
     private NetworkObserver networkObserver;
+
+    private Map<String, String> params;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,19 @@ public class CadastroActivity extends AppCompatActivity implements View.OnClickL
         manager.setNetworkObserver(getCadastroObserver());
     }
 
+    private void loadViews(){
+        edt_user = findViewById(R.id.edt_user);
+        edt_email = findViewById(R.id.edt_email);
+        edt_user_type = findViewById(R.id.edt_user_type);
+
+        sign_in_button = findViewById(R.id.sign_in_button);
+        sign_in_button.setOnClickListener(this);
+        sign_up_button = findViewById(R.id.sign_up_button);
+        sign_up_button.setOnClickListener(this);
+
+        coordinatorLayout = findViewById(R.id.activity_cadastro);
+    }
+
     private NetworkObserver getCadastroObserver() {
         if (networkObserver == null){
             networkObserver = new NetworkObserver() {
@@ -41,7 +67,7 @@ public class CadastroActivity extends AppCompatActivity implements View.OnClickL
 
                 @Override
                 public void doOnGet(String response) {
-                    Log.d("RESPONSE", response);
+
                 }
 
                 @Override
@@ -51,18 +77,11 @@ public class CadastroActivity extends AppCompatActivity implements View.OnClickL
 
                 @Override
                 public void doOnError(String response) {
-                    Log.d("ERRO", response);
+
                 }
             };
         }
         return networkObserver;
-    }
-
-    private void loadViews(){
-        sign_in_button = findViewById(R.id.sign_in_button);
-        sign_in_button.setOnClickListener(this);
-        sign_up_button = findViewById(R.id.sign_up_button);
-        sign_up_button.setOnClickListener(this);
     }
 
     @Override
@@ -74,6 +93,9 @@ public class CadastroActivity extends AppCompatActivity implements View.OnClickL
                 finish();
                 break;
             case R.id.sign_up_button:
+                Toast.makeText(context, "Cadastrado efetuado", Toast.LENGTH_SHORT).show();
+
+
                 startActivity(new Intent(context, DashboardActivity.class)
                         .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
                 finish();
--- app/src/main/java/com/example/personalgamer/LoginActivity.java
+++ app/src/main/java/com/example/personalgamer/LoginActivity.java
@@ -2,30 +2,39 @@ package com.example.personalgamer;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import interfaces.NetworkObserver;
 import network.NetworkManager;
 import util.Mask;
 import util.Path;
+import util.VerifyConnection;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Context context = this;
     private Button sign_in_button, sign_in_switch, sign_up_switch, aluno_switch, personal_switch;
     private View v_user, v_email, v_phone;
-    private EditText edt_phone;
+    private TextInputEditText edt_user, edt_phone, edt_email;
     private CardView crd_switch_type;
-    private boolean change;
+    private boolean typeuser;
+    private CoordinatorLayout coordinatorLayout;
 
     private NetworkManager manager;
     private NetworkObserver networkObserver;
@@ -38,10 +47,35 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         loadViews();
 
         manager = new NetworkManager();
-        manager.setNetworkObserver(getLoginObserver());
+        manager.setNetworkObserver(getRegisterObserver());
     }
 
-    private NetworkObserver getLoginObserver() {
+    private void loadViews(){
+        v_user = findViewById(R.id.v_user);
+        v_email = findViewById(R.id.v_email);
+        v_phone = findViewById(R.id.v_phone);
+
+        edt_user = findViewById(R.id.edt_user);
+        edt_email = findViewById(R.id.edt_email);
+        edt_phone = findViewById(R.id.edt_phone);
+        edt_phone.addTextChangedListener(Mask.insert(edt_phone, "CPFMask"));
+
+        crd_switch_type = findViewById(R.id.crd_switch_type);
+
+        sign_in_button = findViewById(R.id.sign_in_button);
+        sign_in_button.setOnClickListener(this);
+        sign_in_switch = findViewById(R.id.sign_in_switch);
+        sign_in_switch.setOnClickListener(this);
+        sign_up_switch = findViewById(R.id.sign_up_switch);
+        sign_up_switch.setOnClickListener(this);
+
+        aluno_switch = findViewById(R.id.aluno_switch);
+        aluno_switch.setOnClickListener(this);
+        personal_switch = findViewById(R.id.personal_switch);
+        personal_switch.setOnClickListener(this);
+    }
+
+    private NetworkObserver getRegisterObserver() {
         if (networkObserver == null){
             networkObserver = new NetworkObserver() {
                 @Override
@@ -68,33 +102,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         return networkObserver;
     }
 
-    private void loadViews(){
-        v_user = findViewById(R.id.v_user);
-        v_email = findViewById(R.id.v_email);
-        v_phone = findViewById(R.id.v_phone);
-
-        crd_switch_type = findViewById(R.id.crd_switch_type);
-
-        edt_phone = findViewById(R.id.edt_phone);
-        edt_phone.addTextChangedListener(Mask.insert(edt_phone, "CPFMask"));
-
-        sign_in_button = findViewById(R.id.sign_in_button);
-        sign_in_button.setOnClickListener(this);
-        sign_in_switch = findViewById(R.id.sign_in_switch);
-        sign_in_switch.setOnClickListener(this);
-        sign_up_switch = findViewById(R.id.sign_up_switch);
-        sign_up_switch.setOnClickListener(this);
-
-        aluno_switch = findViewById(R.id.aluno_switch);
-        aluno_switch.setOnClickListener(this);
-        personal_switch = findViewById(R.id.personal_switch);
-        personal_switch.setOnClickListener(this);
-    }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.sign_in_switch:
+                typeuser = false;
+                edt_user.setError(null);
+
                 sign_up_switch.setClickable(true);
                 sign_up_switch.setTextColor(Color.parseColor("#DCDCDC"));
                 sign_up_switch.setBackground(ContextCompat.getDrawable(context, R.drawable.rounded_border));
@@ -110,6 +124,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 sign_in_button.setText("Entrar");
                 break;
             case R.id.sign_up_switch:
+                typeuser = true;
+                edt_user.setError(null);
+
                 sign_in_switch.setClickable(true);
                 sign_in_switch.setTextColor(Color.parseColor("#DCDCDC"));
                 sign_in_switch.setBackground(ContextCompat.getDrawable(context, R.drawable.rounded_border));
@@ -143,12 +160,146 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 personal_switch.setBackground(ContextCompat.getDrawable(context, R.drawable.rounded_white));
                 break;
             case R.id.sign_in_button:
-                startActivity(new Intent(context, DashboardActivity.class)
-                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
-                finish();
+                if (!typeuser) {
+                    attemptLogin();
+                } else {
+                    attemptRegistration();
+                }
+
+                break;
+            case R.id.sign_up_button:
+                attemptRegistration();
                 break;
             default:
                 break;
         }
     }
+
+    private void attemptLogin() {
+
+        edt_user.setError(null);
+
+        String user = edt_user.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(user)) {
+            edt_user.setError(getString(R.string.error_field_required));
+            focusView = edt_user;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            if (VerifyConnection.verifyConnection(this)) {
+
+                JSONObject params = new JSONObject();
+                try {
+                    params.put("name", user);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                startActivity(new Intent(context, DashboardActivity.class)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+//                manager.post(params, Path.urlCadastroUsuarios);
+
+//                showProgress(true);
+            } else {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Sem internet", Snackbar.LENGTH_LONG)
+                        .setAction("Conectar", view -> {
+                            WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                            assert wifi != null;
+                            wifi.setWifiEnabled(true);
+                        });
+                snackbar.setActionTextColor(Color.RED);
+                snackbar.show();
+            }
+        }
+    }
+
+    private void attemptRegistration() {
+
+        edt_user.setError(null);
+        edt_phone.setError(null);
+        edt_email.setError(null);
+
+        String user = edt_user.getText().toString();
+        String  phone = Mask.unmask(edt_phone.getText().toString());
+        String email = edt_email.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(user)) {
+            edt_user.setError(getString(R.string.error_field_required));
+            focusView = edt_user;
+            cancel = true;
+        }
+
+        if (typeuser) {
+            if (TextUtils.isEmpty(phone)) {
+                edt_phone.setError(getString(R.string.error_field_required));
+                focusView = edt_phone;
+                cancel = true;
+            } else if (!isPhoneValid(phone)) {
+                edt_phone.setError(getString(R.string.error_invalid_phone));
+                focusView = edt_phone;
+                cancel = true;
+            }
+
+            if (TextUtils.isEmpty(email)) {
+                edt_email.setError(getString(R.string.error_field_required));
+                focusView = edt_email;
+                cancel = true;
+            } else if (!isEmailValid(email)) {
+                edt_email.setError(getString(R.string.error_invalid_email));
+                focusView = edt_email;
+                cancel = true;
+            }
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            if (VerifyConnection.verifyConnection(this)) {
+
+                JSONObject params = new JSONObject();
+                try {
+                    params.put("name", user);
+                    params.put("email", email);
+                    params.put("telephone", phone);
+                    params.put("typeuser", typeuser);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+//                manager.post(params, Path.urlCadastroUsuarios);
+
+//                showProgress(true);
+            } else {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorLayout, "Sem internet", Snackbar.LENGTH_LONG)
+                        .setAction("Conectar", view -> {
+                            WifiManager wifi = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                            assert wifi != null;
+                            wifi.setWifiEnabled(true);
+                        });
+                snackbar.setActionTextColor(Color.RED);
+                snackbar.show();
+            }
+        }
+    }
+
+    private boolean isPhoneValid(String phone) {
+        return phone.length() > 10;
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
 }
