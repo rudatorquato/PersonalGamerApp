1d8aca045649fbe63c199249ba02df4cc9387c74
Rudá Torquato <rudatorquato@gmail.com>
2020-05-27 22:32:17 -0300

    set Personal

--- app/src/main/java/com/example/personalgamer/PersonalActivity.java
+++ app/src/main/java/com/example/personalgamer/PersonalActivity.java
@@ -2,13 +2,85 @@ package com.example.personalgamer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import controller.MeasuresController;
+import controller.UsersController;
+import interfaces.NetworkObserver;
+import modelo.Measures;
+import modelo.Users;
+import network.NetworkManager;
+import util.Path;
 
 public class PersonalActivity extends AppCompatActivity {
+    private Context context = this;
+    private TextView tv_name, tv_email, tv_telephone;
+
+    private Users user;
+
+    private NetworkManager manager;
+    private NetworkObserver networkObserver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal);
+
+        loadViews();
+
+        manager = new NetworkManager();
+        manager.setNetworkObserver(getUserObserver());
+        manager.get(Path.urlGetUsuario);
+    }
+
+    private void loadViews() {
+        tv_name = findViewById(R.id.tv_name);
+        tv_email = findViewById(R.id.tv_email);
+        tv_telephone = findViewById(R.id.tv_telephone);
+
+    }
+
+    private NetworkObserver getUserObserver() {
+        if (networkObserver == null){
+            networkObserver = new NetworkObserver() {
+                @Override
+                public void doOnPost(String response) {
+
+                }
+
+                @Override
+                public void doOnGet(String response) {
+
+                    try {
+                        user = UsersController.getUser(response);
+                        //measures = MeasuresController.getMeasures(response);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (user != null) {
+                        setUser();
+                    }
+                }
+
+                @Override
+                public void doOnError(String response) {
+                }
+            };
+        }
+        return networkObserver;
+    }
+
+    public void setUser() {
+        tv_name.setText(user.getName());
+        tv_email.setText(user.getEmail());
+        tv_telephone.setText(user.getTelephone());
     }
 }
