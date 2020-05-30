45fbf9553a0d2e65046b9b58cee8f9fa694844ae
Leon <leandrobruto42@gmail.com>
2020-05-29 22:20:14 -0300

    choose gifs

--- app/src/main/java/com/example/personalgamer/CadastraExercicioActivity.java
+++ app/src/main/java/com/example/personalgamer/CadastraExercicioActivity.java
@@ -1,16 +1,26 @@
 package com.example.personalgamer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -28,6 +38,8 @@ public class CadastraExercicioActivity extends AppCompatActivity implements View
 
     private TextInputEditText edt_training, edt_place, edt_exercise, edt_image, edt_sequence,
             edt_series, edt_repetitions, edt_charge;
+    private ImageView dialog_image, img_gif;
+    private GridView gridview;
 
     private String training, place, exercise, image, sequence, series,repetitions, charge;
 
@@ -40,6 +52,20 @@ public class CadastraExercicioActivity extends AppCompatActivity implements View
 
     private SharedPreferences preferences;
 
+    private Integer[] imageIDs = {
+            R.drawable.gif_01, R.drawable.gif_02, R.drawable.gif_03, R.drawable.gif_04,
+            R.drawable.gif_05, R.drawable.gif_06, R.drawable.gif_07, R.drawable.gif_08,
+            R.drawable.gif_09, R.drawable.gif_10, R.drawable.gif_11, R.drawable.gif_12,
+            R.drawable.gif_13, R.drawable.gif_14, R.drawable.gif_15, R.drawable.gif_16,
+            R.drawable.gif_17, R.drawable.gif_18, R.drawable.gif_19, R.drawable.gif_20,
+            R.drawable.gif_21, R.drawable.gif_22, R.drawable.gif_23, R.drawable.gif_24,
+            R.drawable.gif_25, R.drawable.gif_26, R.drawable.gif_27, R.drawable.gif_28,
+            R.drawable.gif_29, R.drawable.gif_30, R.drawable.gif_31, R.drawable.gif_32,
+            R.drawable.gif_33, R.drawable.gif_34, R.drawable.gif_35, R.drawable.gif_36,
+            R.drawable.gif_37, R.drawable.gif_38, R.drawable.gif_39, R.drawable.gif_40,
+            R.drawable.gif_41
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,11 +77,13 @@ public class CadastraExercicioActivity extends AppCompatActivity implements View
 
         manager = new NetworkManager();
         manager.setNetworkObserver(getExercicioObserver());
-
-
     }
 
     private void loadViews() {
+        img_gif = findViewById(R.id.img_gif);
+        dialog_image = findViewById(R.id.dialog_image);
+        dialog_image.setOnClickListener(this);
+
         edt_training = findViewById(R.id.edt_training);
         edt_place = findViewById(R.id.edt_place);
         edt_exercise = findViewById(R.id.edt_exercise);
@@ -102,7 +130,9 @@ public class CadastraExercicioActivity extends AppCompatActivity implements View
 
     @Override
     public void onClick(View v) {
-
+        if (v.getId() == R.id.dialog_image) {
+            dialogChooseImages();
+        }
         if (v.getId() == R.id.btn_cadastrar) {
 
             if (attemptRegister()) {
@@ -152,5 +182,72 @@ public class CadastraExercicioActivity extends AppCompatActivity implements View
         }
 
         return true;
+    }
+
+    public class ImageAdapterGridView extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapterGridView(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return imageIDs.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView mImageView;
+
+            if (convertView == null) {
+                mImageView = new ImageView(mContext);
+                mImageView.setLayoutParams(new GridView.LayoutParams(230, 230));
+                mImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                mImageView.setPadding(16, 16, 16, 16);
+            } else {
+                mImageView = (ImageView) convertView;
+            }
+            Glide.with(context).load(context.getResources().getIdentifier(getResources().getResourceEntryName(imageIDs[position]), "drawable", context.getPackageName())).into(mImageView);
+//            mImageView.setImageResource(imageIDs[position]);
+            return mImageView;
+        }
+    }
+
+    private void dialogChooseImages() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = getLayoutInflater();
+        View information = inflater.inflate(R.layout.dialog_choose_image, null);
+
+        gridview = (GridView) information.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapterGridView(getApplicationContext()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent,
+                                    View v, int position, long id) {
+
+                Toast.makeText(context, getResources().getResourceEntryName(imageIDs[position]), Toast.LENGTH_LONG).show();
+                edt_image.setText(getResources().getResourceEntryName(imageIDs[position]));
+                showGif(getResources().getResourceEntryName(imageIDs[position]));
+            }
+        });
+
+        builder.setView(information)
+                .setPositiveButton("Ok", (dialog, id1) -> {
+
+                })
+                .setNegativeButton("Cancelar", (dialog, id2) -> {
+                });
+        builder.create().show();
+    }
+
+    public void showGif(String gif) {
+        Glide.with(context).load(context.getResources().getIdentifier(gif, "drawable", context.getPackageName())).into(img_gif);
     }
 }
